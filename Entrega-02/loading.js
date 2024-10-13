e508ffe8386c140ca1899d6e8b3336b1179317dc
SantiagoG24 <santiagogonzalez_2002@hotmail.com>
2024-10-13 16:16:50 -0300

    loading con url de acceso mejorada

--- Entrega-02/loading.js
+++ Entrega-02/loading.js
@@ -1,4 +1,4 @@
 const carga = document.querySelector(".carga")
 carga.addEventListener("animationend", function () {
-    window.location.href = "http://127.0.0.1:5500/Entrega-02/index.html";
+    window.location.href = "../Entrega-02/index.html";
 });

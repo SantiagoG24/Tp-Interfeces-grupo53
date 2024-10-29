6ef27bb5a3f0381620b04aa4ca1b9c2c470cb2b8
Melina Theill <meli.theill@gmail.com>
2024-10-29 12:14:37 -0300

    fichas-se-mueven

--- Entrega-03/js/juego.js
+++ Entrega-03/js/juego.js
@@ -1,6 +1,9 @@
 btn_play = document.getElementById("btn-play");
 btn_play.addEventListener('click', showConfig);
 let canvas = document.getElementById("myCanvas");
+
+
+
 let ctx = canvas.getContext("2d");
 let canvasWidth = canvas.width;
 let canvasHeight = canvas.height;
@@ -8,97 +11,154 @@ let fichasEnPartida = [];
 let matriz = [];
 let mode = 0;
 let tablero;
-let ficha1;
-
-function showConfig() {
-    let selecionModo = document.getElementById("selecionModo");
-    let selecionFicha = document.getElementById("fichasEleccion");
-    let btn_modo = document.getElementById("btn-modo");
-    let start_game = document.getElementById("start-game");
-
-    btn_modo.addEventListener('click', showSelecionFicha);
-    start_game.addEventListener('click', startGame)
-    selecionModo.addEventListener('click', selecionFicha);
-
-    showMode();
-    function showMode() {
-        selecionModo.classList.toggle("active");
-        let buttonMode4 = document.getElementById("mode4");
-        let buttonMode5 = document.getElementById("mode5");
-        let buttonMode6 = document.getElementById("mode6");
-        let buttonMode7 = document.getElementById("mode7");
-        buttonMode4.addEventListener("click", () => {
-            mode = 4;
-        });
-        buttonMode5.addEventListener("click", () => {
-            mode = 5;
-        });
-        buttonMode6.addEventListener("click", () => {
-            mode = 6;
-        });
-        buttonMode7.addEventListener("click", () => {
-            mode = 7;
-        });
-
-    }
-
-    function showSelecionFicha() {
-        selecionModo.classList.toggle("sacar");
-        selecionFicha.classList.toggle("active");
-    }
-    function startGame() {
-        selecionFicha.classList.toggle("sacar");
-        armarTablero(mode);
-        let imgironman = "assets/ironman-logo.png";
-        let imgcapitan = "captain.america.logo.png";
-        drawFicha("ironman", 45, 45, "red", imgironman);
-        tablero.drawTablero();
+let isMouseDown = false;
+let lastCircleCliked = null;
 
 
-    }
-    function armarTablero() {
-        tablero = new Tablero(ctx, mode, 5, 67);
-    }
-}
-function play() {
-}
-function createTablero(inicioTable) {
-    for (let x = 0; x < filas; x++) {
-        let fila = [];
-        let inicioX = inicioTable;
-        let finX = inicioTable + 105.3;
-        for (let y = 0; y < columnas; y++) {
-            casillero = new Casillero(ctx, inicioX, finX, inicioY, finY);
-            fila.push(casillero);
-            inicioX = inicioX + 105.3;
-            finX = finX + 105.3;
+    function showConfig() {
+        let selecionModo = document.getElementById("selecionModo");
+        let selecionFicha = document.getElementById("fichasEleccion");
+        let btn_modo = document.getElementById("btn-modo");
+        let start_game = document.getElementById("start-game");
+
+        btn_modo.addEventListener('click', showSelecionFicha);
+        start_game.addEventListener('click', startGame)
+        selecionModo.addEventListener('click', selecionFicha);
+
+     showMode();
+        function showMode() {
+            selecionModo.classList.toggle("active");
+            let buttonMode4 = document.getElementById("mode4");
+            let buttonMode5 = document.getElementById("mode5");
+            let buttonMode6 = document.getElementById("mode6");
+            let buttonMode7 = document.getElementById("mode7");
+            buttonMode4.addEventListener("click", () => {
+                mode = 4;
+            });
+            buttonMode5.addEventListener("click", () => {
+                mode = 5;
+            });
+            buttonMode6.addEventListener("click", () => {
+                mode = 6;
+            });
+            buttonMode7.addEventListener("click", () => {
+                mode = 7;
+            });
+
         }
-        matriz.push(fila);
-        inicioY = inicioY + 67;
-        finY = finY + 67;
-    }
-}
 
-function drawFicha(name, x, y, color, img) {
-    ficha = new Circle(name, x, y, 25, color, ctx, img);
-    fichasEnPartida.push(ficha);
-    actualizar();
-}
-function actualizar() {
-    clearCanvas();
-    for (let i = 0; i < fichasEnPartida.length; i++) {
-        fichasEnPartida[i].draw();
-    }
-}
+        function showSelecionFicha() {
+            selecionModo.classList.toggle("sacar");
+            selecionFicha.classList.toggle("active");
+        }
+        function startGame() {
+            selecionFicha.classList.toggle("sacar");
+            armarTablero(mode);
+            let imgironman = "assets/ironman-logo.png";
+            let imgcapitan = "assets/captain-america-logo.png" ;
+            let cantFichas = tablero.getFilas()* tablero.getColumnas();
+            for(i = 0; i<cantFichas;i++){
+                let f1 = drawFicha("ironman", 100, 100+i*10, "red", imgironman);
+                let f2 = drawFicha("ironman", 1100, 100+i*10, "blue", imgcapitan);
+            }
 
-function clearCanvas() {
-    ctx.fillStyle = "black";
-    ctx.fillRect(0, 0, canvasWidth, canvasHeight);
-    for (let i = 0; i < matriz.length; i++) {
-        for (let j = 0; j < matriz[i].length; j++) {
-            matriz[i][j].draw();
-            matriz[i][j].drawObj();
+            tablero.drawTablero();
+        }
+
+        function armarTablero() {
+            tablero = new Tablero(ctx, mode, 5, 67);
+        }
+
+    }
+
+    function play() {
+    }
+
+    function createTablero(inicioTable) {
+        for (let x = 0; x < filas; x++) {
+            let fila = [];
+            let inicioX = inicioTable;
+            let finX = inicioTable + 105.3;
+            for (let y = 0; y < columnas; y++) {
+                casillero = new Casillero(ctx, inicioX, finX, inicioY, finY);
+                fila.push(casillero);
+                inicioX = inicioX + 105.3;
+                finX = finX + 105.3;
+            }
+            matriz.push(fila);
+            inicioY = inicioY + 67;
+            finY = finY + 67;
         }
     }
-}
 
+    function drawFicha(name, x, y, color, img) {
+        let ficha = new Circle(name, x, y, 25, color, ctx, img);
+        fichasEnPartida.push(ficha);
+        actualizar();
+    }
+    function actualizar() {
+        clearCanvas();
+        for (let i = 0; i < fichasEnPartida.length; i++) {
+            fichasEnPartida[i].draw();
+        }
+    }
+
+    function clearCanvas() {
+        ctx.fillStyle = "black";
+        ctx.fillRect(0, 0, canvasWidth, canvasHeight);
+
+        if (tablero) {
+            tablero.drawTablero();
+        }
+       
+      for (let i = 0; i < matriz.length; i++) {
+            for (let j = 0; j < matriz[i].length; j++) {
+                matriz[i][j].draw();
+                matriz[i][j].drawObj();
+            }
+        }
+    }
+
+   
+
+    function findClickedCircle(x,y){
+        for (let i =0; i<fichasEnPartida.length;i++){
+            let circle= fichasEnPartida[i];
+            if (circle.isPointInside(x, y)){
+                return circle;
+            }
+        }
+    }
+
+    
+    canvas.addEventListener("mousedown", onMouseDown, false);
+        function onMouseDown(e) {
+            isMouseDown = true;
+
+            if (lastCircleCliked != null) {
+                lastCircleCliked = null;
+            }
+            let clickFig = findClickedCircle(e.layerX, e.layerY);
+            if (clickFig != null) {
+                lastCircleCliked = clickFig;
+            }
+            actualizar();
+        }
+
+        canvas.addEventListener("mousemove", onMouseMove, false);
+
+        function onMouseMove(e) {
+            if (isMouseDown && lastCircleCliked != null) {
+                lastCircleCliked.setPosition(e.layerX, e.layerY);
+                actualizar();
+            }
+        }
+
+        canvas.addEventListener("mouseup", onMouseUp, false);
+
+        function onMouseUp(e) {
+            lastCircleCliked = null;
+            isMouseDown = false;
+
+            
+        }

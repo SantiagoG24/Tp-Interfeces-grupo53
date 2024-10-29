098f5485936b3f2d9488b6afb911e4ce560d3e6d
SantiagoG24 <santiagogonzalez_2002@hotmail.com>
2024-10-28 23:52:42 -0300

    agregue le menu del usuario

--- Entrega-03/js/index.js
+++ Entrega-03/js/index.js
@@ -2,7 +2,17 @@
 let menu = document.querySelector('#header-menu');
 let nav_bar = document.querySelector('.nav-bar')
 menu.addEventListener('click', menu_desplegable);
-function menu_desplegable(event) {
-    console.log(event)
+function menu_desplegable() {
     nav_bar.classList.toggle("visible")
+}
+
+
+
+//menu del usuario 
+let btnUser = document.querySelector(".header-user");
+let menuUser = document.querySelector(".user-menu")
+
+btnUser.addEventListener('click', desplegarMenuUser);
+function desplegarMenuUser() {
+    menuUser.classList.toggle("visible-user")
 }

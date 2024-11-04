a20b7e3ead115db5aae456715eb09c4dba67a4f9
SantiagoG24 <santiagogonzalez_2002@hotmail.com>
2024-11-04 16:00:07 -0300

    prueba rutas

--- Entrega-03/js/objetosJuego/Casillero.js
+++ Entrega-03/js/objetosJuego/Casillero.js
@@ -1,19 +1,19 @@
 class Casillero {
-    
+
     constructor(ctx, inicioX, finX, inicioY, finY) {
         this.inicioX = inicioX;
         this.finX = finX;
         this.inicioY = inicioY;
         this.finY = finY;
-        this.width = finX - inicioX;      
-        this.height = finY - inicioY;    
+        this.width = finX - inicioX;
+        this.height = finY - inicioY;
         this.ctx = ctx;
         this.ocupado = false;
         this.obj = null;
         this.imgFicha = null;
         this.imgOcupacion = null;
         this.imgCasillero = new Image();
-        this.imgCasillero.src = "../../Entrega-03/assets/casillero.jpg";
+        this.imgCasillero.src = "assets/casillero.jpg";
         this.imgCasillero.onload = () => {
             this.draw();
         };
@@ -29,10 +29,10 @@ class Casillero {
         this.ctx.fillStyle = "#757575";
         this.ctx.fillRect(this.inicioX, this.inicioY, this.width, this.height);
         this.ctx.drawImage(this.imgCasillero, this.inicioX, this.inicioY, this.width, this.height);
-       /* this.ctx.beginPath();
-         this.ctx.arc(this.inicioX + 52.65, this.inicioY + 33.5, 25, 0, 2 * Math.PI);
-         this.ctx.fill();
-         this.ctx.closePath();*/
+        /* this.ctx.beginPath();
+          this.ctx.arc(this.inicioX + 52.65, this.inicioY + 33.5, 25, 0, 2 * Math.PI);
+          this.ctx.fill();
+          this.ctx.closePath();*/
     }
 
     getInicioX() {
@@ -68,31 +68,31 @@ class Casillero {
             const centerX = this.inicioX + this.width / 2;
             const centerY = this.inicioY + this.height / 2;
             const radius = Math.min(this.width, this.height) * 0.3;  // Proporcional al tamaño del casillero
-    
+
             // Dibujar el círculo en el centro del casillero
             this.ctx.beginPath();
             this.ctx.arc(centerX, centerY, radius, 0, 2 * Math.PI);
             this.ctx.fillStyle = this.obj.getFill();
             this.ctx.fill();
             this.ctx.closePath();
-    
+
             // Configurar el recorte circular
             this.ctx.save(); // Guardar el estado del contexto
             this.ctx.beginPath();
             this.ctx.arc(centerX, centerY, radius, 0, 2 * Math.PI);
             this.ctx.clip(); // Recortar al círculo
-    
+
             // Dibujar la imagen ajustada al círculo
             this.imgOcupacion = new Image();
             this.imgOcupacion.src = this.imgFicha;
             this.ctx.drawImage(this.imgOcupacion, centerX - radius, centerY - radius, radius * 2, radius * 2);
-    
+
             // Restaurar el estado del contexto para evitar afectar otros dibujos
             this.ctx.restore();
         }
     }
-    
-    
+
+
     estaEnComlumna(x) {
         return x > this.inicioX && x < this.finX
     }

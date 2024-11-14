bc6b30c2fc27a9a766214a657baa6966004bf82a
SantiagoG24 <santiagogonzalez_2002@hotmail.com>
2024-11-14 20:04:18 -0300

    cards con opacity

--- Entrega-04/app.js
+++ Entrega-04/app.js
@@ -7,8 +7,27 @@ function changeImage() {
     const slider = document.getElementById("slider");
     currentImageIndex = (currentImageIndex + 1) % images.length;
     slider.src = images[currentImageIndex];
-    console.log(images[currentImageIndex]);
+
 }
 
 // Cambia la imagen cada 3 segundos
 setInterval(changeImage, 3000);
+
+
+let arbusto4 = document.getElementsByClassName(".")
+let logo = document.getElementsByClassName(".logo-centro")
+console.log(logo);
+let arbol1 = document.getElementsByClassName(".")
+let piedra2 = document.getElementsByClassName(".")
+let arbusto1 = document.getElementsByClassName(".")
+let arbol2 = document.getElementsByClassName(".")
+let piedra3 = document.getElementsByClassName(".")
+let piedra1 = document.getElementsByClassName(".")
+let arbol3 = document.getElementsByClassName(".")
+let arbusto2 = document.getElementsByClassName(".")
+
+// window.addEventListener('scroll', () => {
+//     let valor = window.scrollY;
+//     logo.style.marginTop = valor * 2.5 + 'px'
+//     logo.style.Top = valor * -1.5 + 'px'
+// })
